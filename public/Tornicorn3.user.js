fa354c96490b365aa8d417f80037cb3e070e201b
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-03-07 01:22:54 -0500

    Add herokuapp to tornicorn @connects.

--- public/Tornicorn3.user.js
+++ public/Tornicorn3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tornicorn3
 // @namespace    sullengenie.Tornicorn3
-// @version      0.2
+// @version      0.3
 // @description  Estimates how difficult torn players are to attack
 // @author       sullengenie[1946152]
 // @require      https://code.jquery.com/jquery-2.2.0.min.js
@@ -9,6 +9,7 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser.min.js
 // @connect      tornicorn.com
 // @connect      localhost
+// @connect      railsicorn.herokuapp.com
 // @include      *.torn.com/factions.php?step=your*
 // @include      *.torn.com/profiles.php?XID=*
 // @grant        GM_addStyle
