4002dc6198c1ac3cfa04724367332012c3970c67
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-03-07 17:35:51 -0500

    Remove param debug prints from controllers.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,16 +1,6 @@
 class PlayersController < ApplicationController
-  # before_action :set_player, only: [:show, :update, :destroy]
 
-  # GET /players
-  # def index
-  #   @players = Player.all
-
-  #   render json: @players
-  # end
-
-  # GET /players/1
   def show
-    puts player_params.inspect
     @user = User.find_by(api_key: player_params[:api_key])
     unless @user
       render json: { error: "Invalid API key #{player_params[:api_key]}" }
@@ -21,31 +11,6 @@ class PlayersController < ApplicationController
     players.each { |player| info_hash[player.torn_id] = player_info(player) }
     render json: info_hash.select { |_, info| info }
   end
-
-  # POST /players
-  # def create
-  #   @player = Player.new(player_params)
-
-  #   if @player.save
-  #     render json: @player, status: :created, location: @player
-  #   else
-  #     render json: @player.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # PATCH/PUT /players/1
-  # def update
-  #   if @player.update(player_params)
-  #     render json: @player
-  #   else
-  #     render json: @player.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # DELETE /players/1
-  # def destroy
-  #   @player.destroy
-  # end
 
   private
 
@@ -60,13 +25,7 @@ class PlayersController < ApplicationController
     relevant_stats
   end
 
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_player
-    #   @player = Player.find(params[:id])
-    # end
-
-    # Only allow a trusted parameter "white list" through.
-    def player_params
-      params.permit(:api_key, ids: [])
-    end
+  def player_params
+    params.permit(:api_key, ids: [])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,6 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users
-  # def index
-  #   @users = User.all
-
-  #   render json: @users
-  # end
-
-  # GET /users/1
-  # def show
-  #   render json: @user
-  # end
-
-  # POST /users
   def create
-    puts user_params.inspect
     api_key = user_params[:api_key]
     @user = User.find_by(api_key: api_key)
     if @user
@@ -30,28 +15,9 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # def update
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
+  private
 
-  # DELETE /users/1
-  # def destroy
-  #   @user.destroy
-  # end
-
-  # private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_user
-    #   @user = User.find(params[:id])
-    # end
-
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.permit(:api_key)
-    end
+  def user_params
+    params.permit(:api_key)
+  end
 end
