dddc6e16ad8aec39b7860b027d0383de671248d4
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-03-07 20:57:12 -0500

    Add hacky faction authorization.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,11 +1,8 @@
 class PlayersController < ApplicationController
+  before_action :authorize_user
 
   def show
-    @user = User.find_by(api_key: player_params[:api_key])
-    unless @user
-      render json: { error: "Invalid API key #{player_params[:api_key]}" }
-      return
-    end
+    @player = @user.player
     info_hash = {}
     players = Player.where(torn_id: player_params[:ids]).includes(:player_info_updates)
     players.each { |player| info_hash[player.torn_id] = player_info(player) }
@@ -15,14 +12,20 @@ class PlayersController < ApplicationController
   private
 
   def player_info(player)
-    difficulty = @user.player.difficulty(player)
-    info = player.player_info_updates.sort_by(&:timestamp).last
+    difficulty = @player.difficulty(player)
+    info = @player.player_info_updates.sort_by(&:timestamp).last
     relevant_stats = { 'difficulty' => difficulty }
     if info
       relevant_stats.merge!(info.attributes.slice('xanax_taken', 'refills', 'stat_enhancers_used'))
     end
     relevant_stats.each { |k, v| relevant_stats[k] = 0 if v.nil? }
     relevant_stats
+  end
+
+  def authorize_user
+    @user = User.find_by(api_key: player_params[:api_key])
+    render json: { error: "Unknown API key #{player_params[:api_key]}" } unless @user
+    render json: { error: "Unauthorized faction" } unless @user.faction.authorized?
   end
 
   def player_params
--- app/models/faction.rb
+++ app/models/faction.rb
@@ -1,4 +1,8 @@
 class Faction < ApplicationRecord
   has_many :users, dependent: :destroy
   has_many :players, dependent: :nullify
+
+  def authorized?
+    torn_id == 16628
+  end
 end
