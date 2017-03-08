3a29b76d96e0ee8baada6f49ac0e4e6af7163d3c
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-03-08 11:10:18 -0500

    Add tampermonkey metadata file.

---
+++ public/Tornicorn3.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Tornicorn3
+// @namespace    sullengenie.Tornicorn3
+// @version      0.5
+// @description  Estimates how difficult torn players are to attack
+// @author       sullengenie[1946152]
+// @require      https://code.jquery.com/jquery-2.2.0.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser.min.js
+// @connect      tornicorn.com
+// @connect      localhost
+// @connect      railsicorn.herokuapp.com
+// @include      *.torn.com/factions.php?step=your*
+// @include      *.torn.com/profiles.php?XID=*
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
