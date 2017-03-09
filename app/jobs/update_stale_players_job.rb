ecebdbe817fa26c3b8aa152969aec602fddbc095
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-03-09 17:57:59 -0500

    Parallelize stale player updates.

--- app/jobs/update_stale_players_job.rb
+++ app/jobs/update_stale_players_job.rb
@@ -3,7 +3,8 @@ class UpdateStalePlayersJob < ActiveJob::Base
     active_users = User.active_users
     stale_players = Player.most_stale_players(active_users.size, 2.weeks.ago)
     request_infos = stale_players.zip(active_users)
-    request_infos.each do |player, user|
+    Parallel.each(request_infos,
+                  in_threads: Rails.application.config.request_threads) do |player, user|
       request = ApiRequest.player_info(user.api_key, player.torn_id)
       api_caller = ApiCaller.new(request, RateLimiter.new(user))
       PlayerInfoUpdater.new(api_caller).call
