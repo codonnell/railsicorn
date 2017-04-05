c21d8d09865208c3ad8c2d850aaa3bad4e1f7e3f
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-04-05 09:42:17 -0400

    Make faction attack updates run in parallel.

--- app/jobs/update_faction_attacks_job.rb
+++ app/jobs/update_faction_attacks_job.rb
@@ -1,6 +1,7 @@
 class UpdateFactionAttacksJob < ActiveJob::Base
   def perform
-    Faction.where.not(api_key: nil).each do |faction|
+    Parallel.each(Faction.where.not(api_key: nil),
+                  in_threads: Rails.application.config.request_threads) do |faction|
       request = ApiRequest.faction_attacks(faction.api_key)
       api_caller = ApiCaller.new(request, NoRateLimiter.new)
       AttacksUpdater.new(api_caller).call
