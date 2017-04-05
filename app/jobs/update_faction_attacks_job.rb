b42c904782a926819f670e6a5be09eda0d754ede
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-04-05 09:52:40 -0400

    Require faction attack updates to use db conn pool.

--- app/jobs/update_faction_attacks_job.rb
+++ app/jobs/update_faction_attacks_job.rb
@@ -2,9 +2,11 @@ class UpdateFactionAttacksJob < ActiveJob::Base
   def perform
     Parallel.each(Faction.where.not(api_key: nil),
                   in_threads: Rails.application.config.request_threads) do |faction|
-      request = ApiRequest.faction_attacks(faction.api_key)
-      api_caller = ApiCaller.new(request, NoRateLimiter.new)
-      AttacksUpdater.new(api_caller).call
+      ActiveRecord::Base.connection_pool.with_connection do
+        request = ApiRequest.faction_attacks(faction.api_key)
+        api_caller = ApiCaller.new(request, NoRateLimiter.new)
+        AttacksUpdater.new(api_caller).call
+      end
     end
   end
 end
