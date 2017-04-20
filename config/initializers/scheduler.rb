f645fb6fb426af563a68e1699315747a9434a9be
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-04-19 21:13:17 -0400

    Remove scheduled updates to test memory usage.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,16 +3,16 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.singleton
 
 # Prevent scheduler from running on rails console and rake tasks
-unless defined?(Rails::Console) || File.split($0).last == 'rake'
-  scheduler.every '6s' do
-    UpdateFactionAttacksJob.perform_now
-  end
+# unless defined?(Rails::Console) || File.split($0).last == 'rake'
+#   scheduler.every '6s' do
+#     UpdateFactionAttacksJob.perform_now
+#   end
 
-  scheduler.every '30s' do
-    UpdateStalePlayersJob.perform_now
-  end
+#   scheduler.every '30s' do
+#     UpdateStalePlayersJob.perform_now
+#   end
 
-  scheduler.every '1m' do
-    IncRequestsAvailableJob.perform_now
-  end
-end
+#   scheduler.every '1m' do
+#     IncRequestsAvailableJob.perform_now
+#   end
+# end
