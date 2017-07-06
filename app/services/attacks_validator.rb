fb476b34409238767437d3011b23784fbc46a47b
Chris O'Donnell <chris.bowdoin@gmail.com>
2017-07-06 09:33:51 -0400

    Allow attacker/defender names to be floats.

--- app/services/attacks_validator.rb
+++ app/services/attacks_validator.rb
@@ -13,11 +13,11 @@ class AttacksValidator < Validator
         required(:timestamp_started) { int? & gt?(0) }
         required(:timestamp_ended) { int? & gt?(0) }
         required(:attacker_id) { str? > empty? | int? & gt?(0) }
-        required(:attacker_name) { none? | str? | int? }
+        required(:attacker_name) { none? | str? | int? | float? }
         required(:attacker_faction) { str? > empty? | int? & gt?(0) }
         required(:attacker_factionname) { none? | str? | int? }
         required(:defender_id) { int? & gt?(0) }
-        required(:defender_name) { str? | int? }
+        required(:defender_name) { str? | int? | float? }
         required(:defender_faction) { str? > empty? | int? & gt?(0) }
         required(:defender_factionname) { none? | str? | int? }
         required(:respect_gain) { int? > eql?(0) | float? & gteq?(0.0) }
